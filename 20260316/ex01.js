ba8c885a1b156d26cd6e9ea81aa79ac402237153
Parkmyounghoi <57881372+dron512@users.noreply.github.com>
2026-03-16 14:28:49 +0900

    commit
    commit

---
+++ 20260316/ex01.js
@@ -0,0 +1,8 @@
+var msg = 'hello';
+console.log(msg);
+
+// cd ~ 사용자 폴더 이동
+// cd 폴더명 해당 폴더로 이동
+// pwd 현재 작업 디렉토리 보기
+// ls 폴더, 디렉토리내에 내용 보기
+// ctrl + shift + ` 터미널 열기
---
+++ 20260316/ex02.js
@@ -0,0 +1,3 @@
+console.log("테스트");
+var a = +'20', b = +'30';
+console.log(a+b);
